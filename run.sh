e3da7784370f97fb354ad4422af68e74e0b2eaa0
Czimber Márk <czimbermark@gmail.com>
2025-10-30 23:34:30 +0000

    stable version 2

---
+++ run.sh
@@ -0,0 +1,16 @@
+docker exec -it master_slam_api bash
+cd rerun_mast3r
+conda activate mast3r-slam
+
+# On local pc:
+# rerun --connect rerun+http://0.0.0.0:9878/proxy
+
+# CLI usage example:
+python sati_master_slam.py \
+  --dataset /workspace/dataset/rgb_no23vcF_69_0 \
+  --config config/base.yaml \
+  --save-as stanford_out \
+  --img-size 512 \
+  --all-frames \
+  --rr-config.headless \
+  --rerun-server-addr master_slam_cli:9878
--- sati_master_slam.py
+++ sati_master_slam.py
@@ -72,21 +72,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-'''
-docker exec -it master_slam_api bash
-cd rerun_mast3r
-conda activate mast3r-slam
 
-# On local pc:
-rerun --connect rerun+http://0.0.0.0:9878/proxy
-
-# CLI usage example:
-python sati_master_slam.py \
-  --dataset /workspace/dataset/rgb_no23vcF_69_0 \
-  --config config/base.yaml \
-  --save-as stanford_out \
-  --img-size 512 \
-  --all-frames \
-  --rr-config.headless \
-  --rerun-server-addr master_slam_cli:9878
-'''
